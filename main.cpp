4ac05005972907ee9264b537df4309f5dd7198db
Adolfo García Vázquez <A01741767@tec.mx>
2024-10-10 12:17:17 -0600

    Pruebaasd

--- main.cpp
+++ main.cpp
@@ -4,5 +4,21 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
 
     cout<<"Probando que esta chingadera jale";
+    cout<<"Probando que esta chingadera jale";
+    cout<<"Probando que esta chingadera jale";
+    cout<<"Probando que esta chingadera jale";
+    cout<<"Probando que esta chingadera jale";
+    cout<<"Probando que esta chingadera jale";
+    cout<<"Probando que esta chingadera jale";
+    cout<<"Probando que esta chingadera jale";
+    cout<<"Probando que esta chingadera jale";
+    cout<<"Probando que esta chingadera jale";
+    cout<<"Probando que esta chingadera jale";
+    cout<<"Probando que esta chingadera jale";
+    cout<<"Probando que esta chingadera jale";
+    cout<<"Probando que esta chingadera jale";
+    cout<<"Probando que esta chingadera jale";
+    cout<<"Probando que esta chingadera jale";
+    cout<<"Probando que esta chingadera jale";
     return 0;
 }
